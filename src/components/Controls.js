9a15c40246627d24dc1858aa30e486b1d5b9f5bf
halamahmoud8 <hala.elzahlan@assesstm.com>
2021-07-15 19:01:12 +0200

    first-edit

---
+++ src/components/Controls.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+ class Controls extends Component {
+
+  render() {
+    const { incrementLabel, decrementLabel, increment, decrement ,reset} = this.props;
+
+    return (
+      <div className="controls">
+        <div onClick={() => {increment();}}>
+          <span>{incrementLabel}</span>
+        </div>
+        <div onClick={() => {decrement();}}>
+          <span>{decrementLabel}</span>
+        </div>
+        <div onClick={() => {reset();}}>
+          <button>Reset</button>
+          {/* { <span>{resetLabel}</span> } */}
+        </div>
+      </div>
+    );
+  }
+}
+
+Controls.propTypes = {
+  increment: PropTypes.func,
+  decrement: PropTypes.func,
+  resetLabel: PropTypes.string,
+  incrementLabel: PropTypes.string,
+  decrementLabel: PropTypes.string,
+};
+
+Controls.defaultProps = {
+  incrementLabel: "+",
+  decrementLabel: "-"
+};
+
+export default Controls;
